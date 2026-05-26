9c05dc43d9c751084a4844ff3f5d9cd71b235535
Giamcito <juan.giraldo03@uceva.edu.co>
2026-05-26 00:05:37 -0500

    Ajustes Generales y mejoras en la interfaz

--- FrontRifas/src/app/pages/dashboard/dashboard.component.ts
+++ FrontRifas/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -10,10 +10,8 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit, OnDestroy {
+export class DashboardComponent implements OnInit {
   user: any;
-  timeRemaining: string = '';
-  private intervalId: any;
 
   constructor(
     private authService: AuthService,
@@ -27,43 +25,11 @@ export class DashboardComponent implements OnInit, OnDestroy {
       }
       this.user = user;
     });
-
-    // Actualizar el contador cada segundo
-    this.updateTimeRemaining();
-    this.intervalId = setInterval(() => {
-      this.updateTimeRemaining();
-    }, 1000);
-  }
-
-  updateTimeRemaining(): void {
-    const remaining = this.authService.getTimeRemaining();
-    
-    if (remaining <= 0) {
-      this.timeRemaining = 'Sesión expirada';
-      this.logout();
-      return;
-    }
-
-    const hours = Math.floor(remaining / (1000 * 60 * 60));
-    const minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
-
-    this.timeRemaining = `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`;
-  }
-
-  pad(num: number): string {
-    return num < 10 ? '0' + num : num.toString();
   }
 
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/']);
-  }
-
-  ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
   }
 }
 
