f3c28418abc33847edb578cba7113941935bb0a5
Giamcito <juan.giraldo03@uceva.edu.co>
2026-05-25 19:20:59 -0500

    Creacion de proyectos Front y Back

---
+++ BackRifas/src/main/java/com/rifas/BackRifas/BackRifasApplication.java
@@ -0,0 +1,13 @@
+package com.rifas.BackRifas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRifasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackRifasApplication.class, args);
+	}
+
+}
---
+++ BackRifas/src/test/java/com/rifas/BackRifas/BackRifasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifas.BackRifas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRifasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
